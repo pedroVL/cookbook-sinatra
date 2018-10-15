ecac5156fa5d3bd98a8ffcf81acd5c08b1115fa2
Pedro Villamor Lopez <pedro88vl@gmail.com>
2018-10-15 17:31:12 +0100

    app not changed

---
+++ app.rb
@@ -0,0 +1,21 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+require "pry-byebug"
+require "better_errors"
+configure :development do
+  use BetterErrors::Middleware
+  BetterErrors.application_root = File.expand_path('..', __FILE__)
+end
+
+get '/' do
+  erb :index
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/' do
+  @usernames = [ 'ssaunier', 'Papillard' ]
+  erb :index
+end
